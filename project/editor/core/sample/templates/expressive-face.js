535d65f99ff5bb864dd3dcbeeefdb2a1475ffd36
glloq <glloq.nz@gmail.com>
2026-09-01 04:06:30 +0200

    Replace face template placeholders with riggable artwork

--- project/editor/core/sample/templates/expressive-face.js
+++ project/editor/core/sample/templates/expressive-face.js
@@ -1,2 +1,2 @@
-import { DEFAULT_SAMPLE_SVG } from '../default-mascot.js';
-export const EXPRESSIVE_FACE_TEMPLATE=Object.freeze({id:'expressive-face',name:'Expressive Face',svg:DEFAULT_SAMPLE_SVG,kind:'expressive'});
+import { EXPRESSIVE_FACE_SVG } from './face-artwork.js';
+export const EXPRESSIVE_FACE_TEMPLATE=Object.freeze({id:'expressive-face',name:'Expressive Face',svg:EXPRESSIVE_FACE_SVG,kind:'expressive'});
